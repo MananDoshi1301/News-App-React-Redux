1aaa4b8e366e836f8a3fa023ea48950a87668281
Manan Doshi <manandoshi1301@gmail.com>
2021-05-09 23:29:19 +0530

    Nav and banner set!

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Banner from './Nav/Banner'
 import Nav from './Nav/Nav'
 
 const HomeScreen = () => {
     return (
         <>
-            {/* Nav    */}
             <Nav />
+            <Banner />
         </>
     )   
 }
