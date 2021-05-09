53e847914226ba2223b38c042dd3101fbb307caa
Manan Doshi <manandoshi1301@gmail.com>
2021-05-09 23:29:04 +0530

    Nav logo set!

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.css"
 import appLogo from '../appLogo.png'
 
 
 const Nav = () => {
+    const [isScrolled, handleIsScrolled] = useState(false);
+    const transitionNavBar = () => {
+        if (window.scrollY > 300) {
+            handleIsScrolled(true);
+        } else {
+            handleIsScrolled(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, [])
     return (
-        <div className="nav">
-            <img className="nav_logo"
-            src={appLogo} alt="" />            
+        <div className={`nav ${isScrolled ? 'nav_dark' : ""}`}>
+            <div className="nav_content">
+                <img className={`nav_logo`}
+                    src={appLogo} alt="" />
+            </div>
         </div>
     )
 }
