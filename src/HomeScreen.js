6edc722771ad400a250357dc6ff5369a7bcfd85a
Manan Doshi <manandoshi1301@gmail.com>
2021-05-10 00:35:38 +0530

    Updated with TopNewsRow

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Banner from './Nav/Banner'
 import Nav from './Nav/Nav'
+import TopNewsRow from './Row/TopNewsRow'
+import requests from "./Requests";
 
 const HomeScreen = () => {
     return (
         <>
             <Nav />
             <Banner />
+            <TopNewsRow 
+            title={'Top Headlines: India'}
+            fetchUrl={requests.fetchTopHeadlinesIndia}
+            />
         </>
     )   
 }
