e6219d6f88cac38748ced7f96547e5cb8a5ade95
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-10 01:51:12 +0530

    top news styling updated

--- src/Row/TopNewsRow.js
+++ src/Row/TopNewsRow.js
@@ -1,32 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import './TopNewsRow.css'
-import axios from '../axios';
+import React, { useEffect, useState } from "react";
+import "./TopNewsRow.css";
+import axios from "../axios";
 
 const TopNewsRow = ({ title, fetchUrl }) => {
-    const [topNews, setTopNews] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setTopNews(request.data.articles);
-            return request;
-        }
+  const [topNews, setTopNews] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setTopNews(request.data.articles);
+      return request;
+    }
 
-        fetchData();
-    }, [])
+    fetchData();
+  }, []);
 
-    console.log(topNews);
-    return (
-        <div className="topNewsRow">
-            <h1 className="topNewsRow_title">{title}</h1>
-            <div className="topNewsRow_posters">    
-                {topNews.map(news => (
-                    <img
-                        className={`topNewsRow_newsPoster`}
-                        src={news.urlToImage} alt={"    "} />
-                ))}
+  console.log(topNews);
+  return (
+    <div className="topNewsRow">
+      <h1 className="topNewsRow_title">{title}</h1>
+      <div className="topNewsRow_posters">
+        {topNews.map((news) => (
+          <div className="topNewsRow_card">
+            <img
+              className={`topNewsRow_newsPoster`}
+              src={news.urlToImage}
+              alt={"    "}
+            />
+            <div className="topNewsRow_cardContent">
+              <h3>{news.title}</h3>
+              <div className="topNewsRow_authorDetails">
+                <span className="topNewsRow_authorDetails_source">
+                  {news?.source.name}
+                </span>
+                <span className="topNewsRow_authorDetails_author">
+                  {news.author ? ", " + news.author : ""}
+                </span>
+              </div>
             </div>
-        </div>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default TopNewsRow
+export default TopNewsRow;
