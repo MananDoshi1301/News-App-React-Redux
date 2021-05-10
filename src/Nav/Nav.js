5513e4c27a50e940aee779954358f484fcbbcba4
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-10 15:27:57 +0530

    Updated with Brand Name

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import "./Nav.css"
-import appLogo from '../appLogo.png'
-
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import appLogo from "../appLogo.png";
 
 const Nav = () => {
-    const [isScrolled, handleIsScrolled] = useState(false);
-    const transitionNavBar = () => {
-        if (window.scrollY > 300) {
-            handleIsScrolled(true);
-        } else {
-            handleIsScrolled(false);
-        }
+  const [isScrolled, handleIsScrolled] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleIsScrolled(true);
+    } else {
+      handleIsScrolled(false);
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener("scroll", transitionNavBar);
-        return () => window.removeEventListener("scroll", transitionNavBar);
-    }, [])
-    return (
-        <div className={`nav ${isScrolled ? 'nav_dark' : ""}`}>
-            <div className="nav_content">
-                <img className={`nav_logo`}
-                    src={appLogo} alt="" />
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+  return (
+    <div className={`nav ${isScrolled ? "nav_dark nav_brandScrolled" : ""}`}>
+      <div className="nav_content">
+        <img className={`nav_logo`} src={appLogo} alt="" />
+        <div className={`nav_brand`}>News Express</div>
+      </div>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
