cc6012b457ea97e758c45f0478f533f6aac3ffd6
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-10 13:55:26 +0530

    TypeWriter effect Added

--- src/Nav/Banner.js
+++ src/Nav/Banner.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './Banner.css';
 import starterImg from '../starterImg4.jpg';
+import Typed from 'react-typed';
 
 const Banner = () => {
     return (
         <header className="banner" style={{
-            backgroundSize:'cover',
-            backgroundImage:`url(${starterImg})`,
-            backgroundPosition:'center center', 
-            }}>
+            backgroundSize: 'cover',
+            backgroundImage: `url(${starterImg})`,
+            backgroundPosition: 'center center',
+        }}>
+
+            <Typed
+                strings={['Find all Your<br> Stories', 'Find all Your<br> Articles', 'Find Your<br> News...!']}
+                typeSpeed={60}
+                backSpeed={50}
+                backDelay={1200}
+                // loop
+                className="banner_TypeWriter"
+                smartBackspace
+            />
         </header>
     )
 }
