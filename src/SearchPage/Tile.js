4f675e32799c4d2f48fe4bbd7893b8eae7bb0c53
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 00:30:14 +0530

    Tiles created

---
+++ src/SearchPage/Tile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Tile.css";
+const Tile = () => {
+    return (
+        <div className="tile">
+            <i class="far fa-futbol"></i>
+            <div>Sports</div>
+        </div>
+    )
+}
+
+export default Tile
