b5bd52fc1227d2b6fbe9933f7a82359cc7c797c0
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 00:07:22 +0530

    Common Margin to div added!

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 import SearchBar from "./SearchBar";
+import SearchTiles from "./SearchTiles";
 
 const SearchPage = () => {
   return (
     <div>
       <Nav />
-      <div style={{position:'absolute', top:'120px'}}>
+      <div style={{position:'absolute', top:'120px', marginLeft:'35px'}}>
         <SearchBar />
+        <SearchTiles />
       </div>
     </div>
   );
