22049e1d435e15cc4e55cd4b9fc23d50cc46b943
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 00:29:59 +0530

    SearchTile created

---
+++ src/SearchPage/SearchTiles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tile from './Tile';
+import './SearchTiles.css';
+
+const SearchTiles = () => {
+    return (
+        <div className="searchTile">
+            <Tile />
+            <Tile />
+            <Tile />
+        </div>
+    )
+}
+
+export default SearchTiles
