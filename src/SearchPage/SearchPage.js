d6dbef5c24e10cb07c23252ccfa6a71c2234a2fa
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 03:31:41 +0530

    Added Categories,  stateful component created

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -4,12 +4,20 @@ import SearchBar from "./SearchBar";
 import SearchTiles from "./SearchTiles";
 
 const SearchPage = () => {
+  const catDetails = [
+    {title: 'Business', icon:'far fa-chart-bar'},
+    {title: 'Entertainment', icon:'far fa-play-circle'},
+    {title: 'General', icon:'fas fa-bullhorn'},
+    {title: 'Health', icon:'fas fa-hospital'},
+    {title: 'Science', icon:'fas fa-flask'},
+    {title: 'Sports', icon:'far fa-futbol'},
+  ]
   return (
     <div>
       <Nav />
-      <div style={{position:'absolute', top:'120px', marginLeft:'35px'}}>
+      <div style={{position:'absolute', top:'120px', marginLeft:'30px', width:'1475px'}}>
         <SearchBar />
-        <SearchTiles />
+        <SearchTiles category={catDetails}/>
       </div>
     </div>
   );
