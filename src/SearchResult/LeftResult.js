361e1a1a8045fb53194493d288f66f068fce35dc
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 20:11:34 +0530

    Created LeftResult

---
+++ src/SearchResult/LeftResult.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./LeftResult.css";
+
+const LeftResult = () => {
+    return (
+        <div className={`leftResult`}>
+            <div className="leftCont">
+            <h1>Genres</h1>
+            <ul>
+                <li>G1</li>
+                <li>G1</li>
+                <li>G1</li>
+                <li>G1</li>
+                <li>G1</li>
+                <li>G1</li>
+            </ul>
+            </div>
+        </div>
+    )
+}
+
+export default LeftResult
