c4b782acade1d1a2b59ce3d40934d5262f747124
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 15:40:47 +0530

    Updated with new categories

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -11,13 +11,27 @@ const SearchPage = () => {
     {title: 'Health', icon:'fas fa-hospital'},
     {title: 'Science', icon:'fas fa-flask'},
     {title: 'Sports', icon:'far fa-futbol'},
+    {title: 'India', icon:'fas fa-rupee-sign'},
+    {title: 'States', icon:'fas fa-flag-usa'},
+    {title: 'BBC News', icon:'fas fa-newspaper'},
+    {title: 'CNN', icon:'fas fa-balance-scale'},
+    {title: 'TechCrunch', icon:'fas fa-microchip'},
+    {title: 'NextBigFuture', icon:'fas fa-atom'},
+    {title: 'Cyrpto Coins', icon:'fab fa-btc'},
+    {title: 'Business Insider', icon:'fas fa-dollar-sign'},
+    {title: 'Wall Street Journal', icon:'far fa-newspaper'},
+    {title: 'MTV', icon:'fas fa-compact-disc'},
+    {title: 'Fox Sports', icon:'fas fa-swimmer'},
+    {title: 'FourFourTwo', icon:'fas fa-futbol'},
   ]
   return (
     <div>
       <Nav />
       <div style={{position:'absolute', top:'120px', marginLeft:'30px', width:'1475px'}}>
         <SearchBar />
-        <SearchTiles category={catDetails}/>
+        <div style={{marginBottom:'40px'}}>
+          <SearchTiles category={catDetails}/>
+        </div>
       </div>
     </div>
   );
