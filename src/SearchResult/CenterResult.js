c427b9878486a18006d57049779f811211536529
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 20:11:18 +0530

    Created CenterResult

---
+++ src/SearchResult/CenterResult.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./CenterResult.css"
+
+const CenterResult = () => {
+    return (
+        <div className={`centerResult`}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam adipisci quisquam porro placeat voluptates commodi eveniet vero, ab, incidunt veritatis accusamus magni itaque fugiat, a illo odio velit vitae quod.
+        </div>
+    )
+}
+
+export default CenterResult
