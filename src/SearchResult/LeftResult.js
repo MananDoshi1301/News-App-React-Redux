044ef2ea4befef514c746c1b2eba435bdf7f5ddb
Manan Doshi <manandoshi1301@gmail.com>
2021-05-13 20:26:25 +0530

    Left section created with list

--- src/SearchResult/LeftResult.js
+++ src/SearchResult/LeftResult.js
@@ -5,15 +5,17 @@ const LeftResult = () => {
     return (
         <div className={`leftResult`}>
             <div className="leftCont">
-            <h1>Genres</h1>
-            <ul>
-                <li>G1</li>
-                <li>G1</li>
-                <li>G1</li>
-                <li>G1</li>
-                <li>G1</li>
-                <li>G1</li>
-            </ul>
+                <h1 className={`leftResult_title`}>Genres</h1>
+                <ul className={`leftResult_list`}>
+                    <li className={`leftResult_listItem`}>G1</li>
+                    <li className={`leftResult_listItem`}>G1</li>
+                    <li className={`leftResult_listItem`}>G1</li>
+                    <li className={`leftResult_listItem`}>G1</li>
+                    <li className={`leftResult_listItem`}>G1</li>
+                    <li className={`leftResult_listItem`}>G1</li>
+                    <li className={`leftResult_listItem`}>G1</li>
+
+                </ul>
             </div>
         </div>
     )
