c8f018ab36529c26ef8440ded978b4e85fe3cf10
Manan Doshi <manandoshi1301@gmail.com>
2021-05-14 16:52:25 +0530

    Link formed

--- src/SearchPage/Tile.js
+++ src/SearchPage/Tile.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import API_KEY from '../API_KEY';
 import "./Tile.css";
-const Tile = ({title, icon, color}) => {
+const Tile = ({title, icon, color, type, link}) => {
+    let linkStr = type+link+'&apiKey='+API_KEY;
     return (
-        <div className={`tile ${color}`}>            
+        <Link to={`/result/${type}/${link}`} className={`tile ${color}`}>            
             <div className='tile_title'>
                 <i className={`${icon}`}></i>&nbsp;{title}
                 <p className='tile_description'></p>
             </div>
-        </div>
+        </Link>
     )
 }
 
