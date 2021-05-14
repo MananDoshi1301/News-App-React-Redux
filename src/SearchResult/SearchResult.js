30dba6c2de105fce6b8689dfbe35dac4d9da77ed
Manan Doshi <manandoshi1301@gmail.com>
2021-05-14 16:54:07 +0530

    Link formation complete

--- src/SearchResult/SearchResult.js
+++ src/SearchResult/SearchResult.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
 import Nav from '../Nav/Nav';
 import LeftResult from './LeftResult';
 import CenterResult from './CenterResult';
 import './SearchResult.css';
+import API_KEY from '../API_KEY';
 
 const SearchResult = () => {
+    const {type, link} = useParams();
+
+    const getLink = () => {
+        let linkStr;
+    if(type === 'sources') {
+        linkStr = type+'?'+link+'apiKey='+API_KEY;
+    }
+    else {
+        linkStr = type+'?'+link+'&apiKey='+API_KEY;
+    } 
+    console.log(linkStr)
+    return linkStr
+    }
+    
+    
     return (
         <div>
             <Nav/>
             <section className={`searchResult`} style={{width:'95%'}}>
                 <LeftResult />
-                <CenterResult />
+                <CenterResult fetchUrl={getLink()}/>
             </section>
         </div>
     )
