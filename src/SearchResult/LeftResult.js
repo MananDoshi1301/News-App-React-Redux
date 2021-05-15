9081b1517096c546dd3195415b2941f3dfb428df
Manan Doshi <manandoshi1301@gmail.com>
2021-05-15 20:16:48 +0530

    Links added!

--- src/SearchResult/LeftResult.js
+++ src/SearchResult/LeftResult.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import "./LeftResult.css";
 import catDetails from "../data";
 
 const LeftResult = () => {
-    console.log(catDetails);
+    let genres = catDetails.map((obj)=>{
+        return obj.category;
+    })
+    genres = [...new Set(genres)];
+    genres = genres.slice(0,genres.length%2+1);
+
+    const items = () => {
+        let arr = [];
+        for(let i=0; i<genres.length; i++){
+            let obj = (
+                <div className="leftResult_header">
+                <div className="leftCont">
+                    <h1 className={`leftResult_title`}>{genres[i][0].toUpperCase()+genres[i].slice(1).toLowerCase()} . . .</h1>
+                    <ul className={`leftResult_list`}>
+                        {catDetails
+                            .filter((category) => {
+                                return category.category === genres[i];
+                            })
+                            .map((category) => {
+                                return (
+                                    <li className={`leftResult_listItem`}><Link to={`/result/${category.type}/${category.link}`}>{category.title}</Link></li>
+                                )
+                            })}
+                    </ul>
+                </div>
+            </div> 
+            )
+            arr.push(obj);
+        }
+        return arr;
+    }
+
     return (
         <>
             <div className={`leftResult`}>
-                <div className="leftResult_header">
+                {/* <div className="leftResult_header">
                     <div className="leftCont">
                         <h1 className={`leftResult_title`}>Genres</h1>
                         <ul className={`leftResult_list`}>
@@ -22,12 +54,13 @@ const LeftResult = () => {
                                 })}
                         </ul>
                     </div>
-                </div>
-
+                </div> */}
+                
+                {items().map(obj=>obj)}
                 
                 
             </div>
-
+            
 
 
         </>
--- src/SearchResult/RightResult.js
+++ src/SearchResult/RightResult.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import "./RightResult.css";
 import catDetails from "../data";
 
 const RightResult = () => {
-    console.log(catDetails);
+    let genres = catDetails.map((obj)=>{
+        return obj.category;
+    })
+    genres = [...new Set(genres)];
+    genres = genres.slice(genres.length%2+1);
+
+    const items = () => {
+        let arr = [];
+        for(let i=0; i<genres.length; i++){
+            let obj = (
+                <div className="rightResult_header">
+                    <div className="rightCont">
+                        <h1 className={`rightResult_title`}>{genres[i][0].toUpperCase()+genres[i].slice(1).toLowerCase()} . . .</h1>
+                        <ul className={`rightResult_list`}>
+                            {catDetails
+                                .filter((category) => {
+                                    return category.category === genres[i];
+                                })
+                                .map((category) => {
+                                    return (
+                                        <li className={`rightResult_listItem`}><Link to={`/result/${category.type}/${category.link}`}>{category.title}</Link></li>
+                                    )
+                                })}
+                        </ul>
+                    </div>
+                </div>
+            )
+            arr.push(obj);
+        }
+        return arr;
+    }
     return (
         <>
             <div className={`rightResult`}>
-                <div className="rightResult_header">
+                {/* <div className="rightResult_header">
                     <div className="rightCont">
                         <h1 className={`rightResult_title`}>From The 'GIANTS'</h1>
                         <ul className={`rightResult_list`}>
@@ -22,9 +53,9 @@ const RightResult = () => {
                                 })}
                         </ul>
                     </div>
-                </div>
+                </div> */}
 
-                
+                {items().map(obj=>obj)}
                 
             </div>
 
