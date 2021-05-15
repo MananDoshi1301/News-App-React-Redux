fab3444700a2495865bbfccb3c9d32bfdf432975
Manan Doshi <manandoshi1301@gmail.com>
2021-05-15 16:09:23 +0530

    LeftResult complete

--- src/SearchResult/LeftResult.js
+++ src/SearchResult/LeftResult.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import "./LeftResult.css";
+import catDetails from "../data";
 
 const LeftResult = () => {
+    console.log(catDetails);
     return (
-        <div className={`leftResult`}>
-            <div className="leftCont">
-                <h1 className={`leftResult_title`}>Genres</h1>
-                <ul className={`leftResult_list`}>
-                    <li className={`leftResult_listItem`}>G1</li>
-                    <li className={`leftResult_listItem`}>G1</li>
-                    <li className={`leftResult_listItem`}>G1</li>
-                    <li className={`leftResult_listItem`}>G1</li>
-                    <li className={`leftResult_listItem`}>G1</li>
-                    <li className={`leftResult_listItem`}>G1</li>
-                    <li className={`leftResult_listItem`}>G1</li>
+        <>
+            <div className={`leftResult`}>
+                <div className="leftResult_header">
+                    <div className="leftCont">
+                        <h1 className={`leftResult_title`}>Genres</h1>
+                        <ul className={`leftResult_list`}>
+                            {catDetails
+                                .filter((category) => {
+                                    return category.category === "genre"
+                                })
+                                .map((category) => {
+                                    return (
+                                        <li className={`leftResult_listItem`}>{category.title}</li>
+                                    )
+                                })}
+                        </ul>
+                    </div>
+                </div>
 
-                </ul>
+                
+                
             </div>
-        </div>
+
+
+
+        </>
     )
 }
 
