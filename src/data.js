f53bd2f81e398dc3b4db1aec9b15c29afd2f9281
Manan Doshi <manandoshi1301@gmail.com>
2021-05-15 15:23:13 +0530

    Central category file!

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const catDetails = [
+    {title: 'Business', icon:'far fa-chart-bar', type:"everything", link:'q=business', category:"genre"},
+    {title: 'Entertainment', icon:'far fa-play-circle', type:"everything", link:'q=entertainment', category:"genre"},
+    {title: 'General', icon:'fas fa-bullhorn', type:"everything", link:'q=general', category:"genre"},
+    {title: 'Health', icon:'fas fa-hospital', type:"everything", link:'q=health', category:"genre"},
+    {title: 'Science', icon:'fas fa-flask', type:"everything", link:'q=science', category:"genre"},
+    {title: 'Sports', icon:'far fa-futbol', type:"everything", link:'q=sports', category:"genre"},
+    {title: 'Sports', icon:'fas fa-code', type:"everything", link:'q=technology', category:"genre"},
+    {title: 'India', icon:'fas fa-rupee-sign', type:"everything", link:'q=in', category:"genre"},
+    {title: 'States', icon:'fas fa-flag-usa', type:"everything", link:'q=us', category:"genre"},
+    {title: 'BBC News', icon:'fas fa-newspaper', type:"top-headlines", link:'sources=bbc-news', category:"giants"},
+    {title: 'CNN', icon:'fas fa-balance-scale', type:"top-headlines", link:'sources=cnn', category:"giants"},
+    {title: 'TechCrunch', icon:'fas fa-microchip', type:"top-headlines", link:'sources=techcrunch', category:"giants"},
+    {title: 'National Geographic', icon:'fas fa-atom', type:"top-headlines", link:'sources=national-geographic', category:"giants"},
+    {title: 'Cyrpto Coins', icon:'fab fa-btc', type:"top-headlines", link:'sources=crypto-coins-news', category:"giants"},
+    {title: 'Business Insider', icon:'fas fa-dollar-sign', type:"top-headlines", link:'sources=business-insider', category:"giants"},
+    {title: 'Wall Street Journal', icon:'far fa-newspaper', type:"top-headlines", link:'sources=the-wall-street-journal', category:"giants"},
+    {title: 'MTV', icon:'fas fa-compact-disc', type:"top-headlines", link:'sources=mtv-news', category:"giants"},
+    {title: 'Fox Sports', icon:'fas fa-swimmer', type:"top-headlines", link:'sources=fox-sports', category:"giants"},
+    {title: 'FourFourTwo', icon:'fas fa-futbol', type:"top-headlines", link:'sources=four-four-two', category:"giants"},
+  ]
+
+  export default catDetails;
