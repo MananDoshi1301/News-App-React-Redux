d18f418769cd3fe001f738fe23f84f6b39fcf57c
Manan Doshi <manandoshi1301@gmail.com>
2021-05-15 21:48:02 +0530

    Added ScrollBtn!

--- src/SearchResult/SearchResult.js
+++ src/SearchResult/SearchResult.js
@@ -4,19 +4,20 @@ import Nav from '../Nav/Nav';
 import LeftResult from './LeftResult';
 import RightResult from './RightResult';
 import CenterResult from './CenterResult';
+import ScrollBackBtn from '../ScrollBackBtn';
 import './SearchResult.css';
 import API_KEY from '../API_KEY';
 
 const SearchResult = () => {
     const { type, link } = useParams();
-
     const getLink = () => {
         let linkStr;
         linkStr = type + '?' + link + '&apiKey=' + API_KEY;
         console.log(linkStr)
         return linkStr
     }
-
+    
+    window.scrollTo(0,0);
 
     return (
         <div>
@@ -25,6 +26,7 @@ const SearchResult = () => {
                 <LeftResult />
                 <CenterResult fetchUrl={getLink()} type={type} />
                 <RightResult />
+                <ScrollBackBtn />
             </section>
         </div>
     )
