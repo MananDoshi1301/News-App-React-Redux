a8bf2db9a0c325db50ec250283fd165d2a426193
Manan Doshi <manandoshi1301@gmail.com>
2021-05-15 13:22:37 +0530

    SearchTile click and redirect results configred succesfully!

--- src/SearchResult/CenterResult.js
+++ src/SearchResult/CenterResult.js
@@ -1,49 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import axios from "../axios";
 import "./CenterResult.css"
-import {motion} from 'framer-motion';
+import appLogo from "../appLogo.png";
+import { motion } from 'framer-motion';
 
-const CenterResult = ({fetchUrl}) => {
+const CenterResult = ({ fetchUrl, type }) => {
     // const [open, setOpen] = useState(false);
     const [news, setNews] = useState([]);
 
-    useEffect(()=>{
-        async function fetchData(){
+    useEffect(() => {
+        async function fetchData() {
             const request = await axios.get(fetchUrl);
+            if (type === 'sources') setNews(request.data.sources);
+            else setNews(request.data.articles);
             console.log(request);
-            setNews(request.data.articles);
         }
-
+        console.log('In useEffect!')
         fetchData();
-    },[fetchUrl]);
+    }, [fetchUrl]);
 
     return (
         <div className={`centerResult`}>
-            {news.map((singNews)=>{
-                return <motion.div className="resultCard">
-                <div className="resultCard_img">
-                    <img src={`${singNews.urlToImage}`} alt="" />
-                </div>
-                <div className="resultCard_content" >
-                    <a href={singNews.url} target="_blank">
-                    <div className="resultCard_title">
-                        <h1>
-                            {singNews.title}
-                        </h1>
-                    </div>                    
-                    <div className={`resultCard_description`}>
-                        {singNews.description}
+            <div className="centreResult_header">
+                
+            </div>
+            {news && news.map((singNews) => {
+                const time = new Date(singNews.publishedAt);
+                const options = { month: 'long' };
+                const month = new Intl.DateTimeFormat('en-US', options).format(time);
+                return <div className="resultCard">
+                    <div className="resultCard_img">
+                        <img src={`${singNews.urlToImage?singNews.urlToImage:""}`} alt="" />
                     </div>
-                    <div className="resultCard_sourceRow">
-                    <small className="resultCard_source">
-                        {singNews.source.name}
-                    </small>                    
+                    <div className="resultCard_content" >
+                        <a href={singNews.url} target="_blank">
+                            <div className="resultCard_title">
+                                <h1>
+                                    {singNews.title}
+                                </h1>
+                            </div>
+                            <div className={`resultCard_description`}>
+                                {singNews.description}
+                            </div>
+                            <div className="resultCard_sourceRow">
+                                <small className="resultCard_source">
+                                    {singNews.source.name}
+                                </small>
+                                <small>
+                                    {month + ' ' + time.getDate() + ', ' + time.getFullYear()}
+                                </small>
+                            </div>
+                        </a>
                     </div>
-                    </a>
                 </div>
-            </motion.div>
             })}
-            
+
         </div>
     )
 }
--- src/SearchResult/SearchResult.js
+++ src/SearchResult/SearchResult.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Nav from '../Nav/Nav';
 import LeftResult from './LeftResult';
 import CenterResult from './CenterResult';
@@ -7,27 +7,22 @@ import './SearchResult.css';
 import API_KEY from '../API_KEY';
 
 const SearchResult = () => {
-    const {type, link} = useParams();
+    const { type, link } = useParams();
 
     const getLink = () => {
         let linkStr;
-    if(type === 'sources') {
-        linkStr = type+'?'+link+'apiKey='+API_KEY;
+        linkStr = type + '?' + link + '&apiKey=' + API_KEY;
+        console.log(linkStr)
+        return linkStr
     }
-    else {
-        linkStr = type+'?'+link+'&apiKey='+API_KEY;
-    } 
-    console.log(linkStr)
-    return linkStr
-    }
-    
-    
+
+
     return (
         <div>
-            <Nav/>
-            <section className={`searchResult`} style={{width:'95%'}}>
+            <Nav />
+            <section className={`searchResult`} style={{ width: '95%' }}>
                 <LeftResult />
-                <CenterResult fetchUrl={getLink()}/>
+                <CenterResult fetchUrl={getLink()} type={type} />
             </section>
         </div>
     )
