90334831d882d70f994475b16b9c5a0876594656
Manan Doshi <manandoshi1301@gmail.com>
2021-05-16 20:09:47 +0530

    Removed unnecessary code

--- src/Articles/Articles.js
+++ src/Articles/Articles.js
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from 'react'
-// import { nytKey } from '../API_KEY';
-// import { nyt } from '../axios';
+import React, { useEffect, useState } from 'react';
+import ScrollBackBtn from '../ScrollBackBtn';
 
 import Nav from '../Nav/Nav';
 import ArticleFormat from './ArticleFormat';
 
 const Articles = ({ fetchUrl = 'viewed/30.json' }) => {
-  // const [userArticles, setUserArticles] = useState([]);
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const req = await nyt.mostPopular.get(fetchUrl + '?api-key=' + nytKey);
-  //       setUserArticles(req.data.results);
-  //       console.log(req);
-  //     } catch (error) {
-  //       console.log(error.message);
-  //     }
-  //   }
-  //   fetchData();
-  // }, [fetchUrl])
+  
   return (
     <>
       <Nav />
       <div>
         <ArticleFormat fetchUrl={'/viewed/7.json'} />
+        <ScrollBackBtn />
       </div>
     </>
   )
