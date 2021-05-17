82ff0be979914712bd717b15e3c2842f080f7fd2
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-17 13:17:02 +0530

    temporary pageSize added 40!

--- src/Requests.js
+++ src/Requests.js
@@ -1,7 +1,7 @@
 import API_KEY from './API_KEY';
 
 const requests = {
-    fetchTopHeadlinesIndia: `top-headlines?country=in&apiKey=${API_KEY}`,
+    fetchTopHeadlinesIndia: `top-headlines?country=in&pageSize=40&apiKey=${API_KEY}`,
     fetchTopHeadlinesUS: `top-headlines?country=us&apiKey=${API_KEY}`,
 }
 
