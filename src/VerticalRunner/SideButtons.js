da0a09a26d5c9cf9754ecad63e3dac02ccb788f8
Manan Doshi <manandoshi1301@gmail.com>
2021-05-17 17:42:04 +0530

    SideButtons created

---
+++ src/VerticalRunner/SideButtons.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './SideButtons.css'
+import {topDetails} from '../data';
+import API_KEY from '../API_KEY';
+
+const SideButtons = ({headline, setHeadline}) => {
+    return (
+        <div className='sideButtons'>
+            <span>
+            <div className="sideButtons_title">Select For Headline!</div>
+            <div className="sideButtons_panel">
+                {topDetails.map((obj)=>{
+                    return (
+                        <div className={`sideButttons_btn 
+                        ${obj.title === headline[1]?'btnActive':''}
+                        `} onClick={()=>{
+                            setHeadline([`top-headlines?${obj.genre}=${obj.value}&apiKey=${API_KEY}`, obj.title])
+                        }}><span className={'btn'}>{obj.title}</span></div>
+                    )            
+                })}            
+            </div> </span>
+            <span></span>
+        </div>
+    )
+}
+
+export default SideButtons
