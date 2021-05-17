ea821acbb6e95fc5314294a7e6e9fe8fcd28c3c9
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-18 00:19:19 +0530

    Text Formatted!

--- src/SearchPage/SearchBar.js
+++ src/SearchPage/SearchBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './SearchBar.css';
 
-const SearchBar = ({search, clickSearch}) => {
+const SearchBar = ({ search, clickSearch }) => {
     return (
         <div className="searchBar">
-            <input ref={search} className="searchBar_input" type="text"/>
+            <input ref={search} className="searchBar_input" type="text" />
             <button className="searchBar_button" onClick={clickSearch}><i class="fas fa-search"></i></button>
         </div>
     )
