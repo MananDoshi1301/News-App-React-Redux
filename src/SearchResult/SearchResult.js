ea0968655af2c3ff76907716cf261cd39c4d20fa
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-17 23:47:14 +0530

    Code formatted!

--- src/SearchResult/SearchResult.js
+++ src/SearchResult/SearchResult.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import Nav from '../Nav/Nav';
-import LeftResult from './LeftResult';
-import RightResult from './RightResult';
-import CenterResult from './CenterResult';
-import ScrollBackBtn from '../ScrollBackBtn';
-import './SearchResult.css';
-import API_KEY from '../API_KEY';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Nav from "../Nav/Nav";
+import LeftResult from "./LeftResult";
+import RightResult from "./RightResult";
+import CenterResult from "./CenterResult";
+import ScrollBackBtn from "../ScrollBackBtn";
+import "./SearchResult.css";
+import API_KEY from "../API_KEY";
 
 const SearchResult = () => {
-    const { type, link } = useParams();
-    const getLink = () => {
-        let linkStr;
-        linkStr = type + '?' + link + '&apiKey=' + API_KEY;
-        console.log(linkStr)
-        return linkStr
-    }
-    
-    window.scrollTo(0,0);
+  const { type, link } = useParams();
+  const getLink = () => {
+    let linkStr;
+    linkStr = type + "?" + link + "&apiKey=" + API_KEY;
+    console.log(linkStr);
+    return linkStr;
+  };
 
-    return (
-        <div>
-            <Nav />
-            <section className={`searchResult`} style={{ width: '95%' }}>
-                <LeftResult />
-                <CenterResult fetchUrl={getLink()} type={type} />
-                <RightResult />
-                <ScrollBackBtn />
-            </section>
-        </div>
-    )
-}
+  window.scrollTo(0, 0);
 
-export default SearchResult
+  return (
+    <div>
+      <Nav />
+      <section className={`searchResult`} style={{ width: "95%" }}>
+        <LeftResult />
+        <CenterResult fetchUrl={getLink()} type={type} />
+        <RightResult />
+        <ScrollBackBtn />
+      </section>
+    </div>
+  );
+};
+
+export default SearchResult;
